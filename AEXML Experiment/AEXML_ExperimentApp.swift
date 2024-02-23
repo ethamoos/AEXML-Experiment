fa7f6137ccbf69a4469b8a1b88ca5903a1c345c5
ethamoos <amosdeane@gmail.com>
2024-02-23 19:02:06 +0000

    Initial Commit

---
+++ AEXML Experiment/AEXML_ExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AEXML_ExperimentApp.swift
+//  AEXML Experiment
+//
+//  Created by Amos Deane on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AEXML_ExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
